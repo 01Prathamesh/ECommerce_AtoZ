39e3f0e08f2e12b2eee63081de845d2fb600c9df
Prathamesh Kasar <prathameshkasar000@gmail.com>
2024-11-04 11:41:00 +0530

    urls of all pages are created and added in the urls.py file of shop's app

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import homepage
+from .views import homepage, product_listing, product_detail, checkout, payment_success, payment_failure
 
 urlpatterns = [
     path('', homepage, name='homepage'),
+    path('products/', product_listing, name='product_listing'),
+    path('products/<int:product_id>/', product_detail, name='product_detail'),
+    path('checkout/', checkout, name='checkout'),
+    path('payment/success/', payment_success, name='payment_success'),
+    path('payment/failure/', payment_failure, name='payment_failure'),
 ]
