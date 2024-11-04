7a65072c865c134adaaddc3faa17307e551b25b5
Prathamesh Kasar <prathameshkasar000@gmail.com>
2024-11-04 13:05:53 +0530

    updated little bugs

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+# shop/admin.py
+from django.contrib import admin
+from .models import Category, Product
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug') 
+    search_fields = ('name', 'slug')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category', 'price', 'stock', 'is_active', 'is_featured')
+    search_fields = ('name', 'category__name')
+    list_filter = ('category', 'is_active', 'is_featured')
