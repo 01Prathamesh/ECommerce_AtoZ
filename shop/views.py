2b5125c4bbeb690a57c7da8e19479162ce994126
Prathamesh Kasar <prathameshkasar000@gmail.com>
2024-11-04 10:58:44 +0530

    homepage view is created in the views.py file of the app

--- shop/views.py
+++ shop/views.py
@@ -1,1 +1,11 @@
 from django.shortcuts import render
+from .models import Product
+
+def homepage(request):
+    # Retrieve featured products
+    featured_products = Product.objects.filter(is_active=True, is_featured=True)
+
+    context = {
+        'featured_products': featured_products
+    }
+    return render(request, 'homepage.html', context)
