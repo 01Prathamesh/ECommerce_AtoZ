9c1e525d37c416d8101447afa8a24e6a1cc05963
Prathamesh Kasar <prathameshkasar000@gmail.com>
2024-11-05 12:16:04 +0530

    Added permission to vercel

--- ECommerce_AtoZ/settings.py
+++ ECommerce_AtoZ/settings.py
@@ -133,3 +133,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'products')
+
+#For allowing our Django application to accept requests from any subdomain of vercel.app
+ALLOWED_HOSTS = ['.vercel.app']
