355722cd3a581184e92d249ff2c46627cec0fb91
Prathamesh Kasar <prathameshkasar000@gmail.com>
2024-11-06 16:25:41 +0530

    Updated to host on local server

--- ECommerce_AtoZ/wsgi.py
+++ ECommerce_AtoZ/wsgi.py
@@ -1,5 +1,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
+from django.core.handlers.wsgi import WSGIRequest
+from django.http import HttpResponse
 
 # Set the default settings module for the 'DJANGO_SETTINGS_MODULE' environment variable.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ECommerce_AtoZ.settings')
@@ -9,4 +11,24 @@ application = get_wsgi_application()
 
 # Vercel expects either 'app' or 'handler'.
 # Assign the WSGI application callable to 'handler'.
-handler = application
+def handler(request):
+    # Convert the Vercel request into a Django WSGIRequest
+    environ = {
+        'REQUEST_METHOD': request.method,
+        'PATH_INFO': request.path,
+        'QUERY_STRING': request.query_string,
+        'CONTENT_TYPE': request.headers.get('Content-Type', ''),
+        'CONTENT_LENGTH': str(request.headers.get('Content-Length', 0)),
+        'wsgi.input': request.body,
+        'SERVER_NAME': 'localhost',
+        'SERVER_PORT': '8000',
+    }
+
+    # Create a Django request object
+    django_request = WSGIRequest(environ)
+
+    # Call the WSGI application (Django)
+    response = application(django_request)
+
+    # Return a simple HttpResponse
+    return HttpResponse(response.content, status=response.status_code, content_type=response.get('Content-Type'))
